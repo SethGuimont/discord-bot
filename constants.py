a36b8ac33100c09bc1478cbb49ddf8c139aa2d68
Seth Guimont <sethguimont@outlook.com>
2023-05-29 17:31:51 -0500

    Added Moderation and Logging

---
+++ constants.py
@@ -0,0 +1,6 @@
+MULTIPLAYER_MODES = [
+    'Frontline', 'Team Deathmatch', 'Search and Destroy', 'Domination', 'Free-For-All',
+    'Kill Confirmed', 'Hardpoint', 'Gunfight',
+]
+
+BANNED_WORDS = ["faggot", "nigger", "transvestite", "tranny", "spic", "spick", "spik", "spig", "spigotty", "chink"]
--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import random
 
-MULTIPLAYER_MODES = [
-    'Frontline', 'Team Deathmatch', 'Search and Destroy', 'Domination', 'Free-For-All',
-    'Kill Confirmed', 'Hardpoint', 'Gunfight',
-]
+from constants import *
+
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
+
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
@@ -22,9 +21,32 @@ async def on_ready():
 
 
 @bot.event
+async def on_message_join(member):
+    channel = bot.get_channel(1109948451577401347)
+    embed = discord.Embed(title=f"Welcome {member.name}",
+                          description=f"Thanks for joining {member.guild.name}!")  # F-Strings!
+    embed.set_thumbnail(url=member.avatar_url)  # Set the embed's thumbnail to the member's avatar image!
+    await channel.send(embed=embed)
+
+
+@bot.event
 async def on_message(message):
     if message.channel.name == 'general':
+        for i in BANNED_WORDS:  # Go through the list of bad words;
+            if i in message.content:
+                await message.delete()
+                await message.channel.send(f"{message.author.mention} Don't use that word here!")
+                bot.dispatch('profanity', message, i)
+                return  # So that it doesn't try to delete the message again.
         await bot.process_commands(message)
+
+
+@bot.event
+async def on_profanity(message, word):
+    channel = bot.get_channel(1112860393874923591)
+    embed = discord.Embed(title="Profanity Alert!", description=f"{message.author.name} just said ||{word}||",
+                          color=discord.Color.blurple())  # Let's make an embed!
+    await channel.send(embed=embed)
 
 
 @bot.command()
